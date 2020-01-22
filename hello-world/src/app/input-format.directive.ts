f7835a94e91896541d41598fd35d874ad042ce99
Mariusz Krzysztof Przybysz <mariusz.krzysztof.przybysz@gmail.com>
2020-01-22 12:03:12 +0100

    Creating custom directives - v1

---
+++ hello-world/src/app/input-format.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, HostListener, ElementRef, Input } from '@angular/core';
+import { FormatInputPathObject } from 'path';
+
+@Directive({
+  selector: '[appInputFormat]'
+})
+export class InputFormatDirective {
+  constructor(private _el: ElementRef) { }
+
+  // @HostListener('focus') onFocus() {
+  //   console.log('onFocus');
+  // }
+
+  @Input('format') format;
+  @HostListener('blur') onBlur() {
+    let value: string = this._el.nativeElement.value;
+
+    if (this.format == 'lowercase') {
+      this._el.nativeElement.value = value.toLowerCase();
+    }
+    else {
+      this._el.nativeElement.value = value.toUpperCase();
+    }
+  }
+}
