024b7f1096f2b4fc928b91fbad6319d7ba7a360e
Mariusz Krzysztof Przybysz <mariusz.krzysztof.przybysz@gmail.com>
2020-01-23 12:40:12 +0100

    FormArray

---
+++ hello-world/src/app/new-course-form/new-course-form.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-new-course-form',
+  templateUrl: './new-course-form.component.html',
+  styleUrls: ['./new-course-form.component.css']
+})
+export class NewCourseFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  form = new FormGroup({
+    topics: new FormArray([])
+  });
+
+  addTopic(topic: HTMLInputElement) {
+    this.topics.push(new FormControl(topic.value));
+    topic.value = '';
+  }
+
+  get topics(): FormArray {
+    return this.form.get('topics') as FormArray;
+  }
+
+  removeTopic(topic: FormControl): void {
+    var index = this.topics.controls.indexOf(topic);
+    this.topics.removeAt(index);
+  }
+}
