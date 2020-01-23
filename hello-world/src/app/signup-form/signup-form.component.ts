1757ec7947f2d8014b597e222fbe52a23e18e219
Mariusz Krzysztof Przybysz <mariusz.krzysztof.przybysz@gmail.com>
2020-01-23 10:22:25 +0100

    Adding Validation

--- hello-world/src/app/signup-form/signup-form.component.ts
+++ hello-world/src/app/signup-form/signup-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'signup-form',
@@ -8,7 +9,15 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class SignupFormComponent {
   form = new FormGroup({
-    username: new FormControl(),
-    password: new FormControl()
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
   });
+
+  get username(): AbstractControl {
+    return this.form.get('username');
+  }
+
+  get password(): AbstractControl {
+    return this.form.get('password');
+  }
 }
