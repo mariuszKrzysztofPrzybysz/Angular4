d8f197d395832beb1915855b828618600345d61a
Mariusz Krzysztof Przybysz <m.przybysz@storware.eu>
2020-01-24 22:34:12 +0100

    Handling Expected Errors

--- hello-world/src/app/posts/posts.component.ts
+++ hello-world/src/app/posts/posts.component.ts
@@ -39,9 +39,14 @@ export class PostsComponent implements OnInit {
           post['id'] = response.json().id;
           this.posts.splice(0, 0, post);
         },
-        error => {
-          alert('An unexpected error occured');
-          console.log(error);
+        (error: Response) => {
+          if (error.status === 400) {
+            // this.form.setErrors(error.json());
+          }
+          else {
+            alert('An unexpected error occured');
+            console.log(error);
+          }
         });
   }
 
@@ -67,9 +72,14 @@ export class PostsComponent implements OnInit {
           let index = this.posts.indexOf(post);
           this.posts.splice(index, 1);
         },
-        error => {
-          alert('An unexpected error occured');
-          console.log(error);
+        (error: Response) => {
+          if (error.status === 404) {
+            alert('This post has already been deleted');
+          }
+          else {
+            alert('An unexpected error occured');
+            console.log(error);
+          }
         });
   }
 }
