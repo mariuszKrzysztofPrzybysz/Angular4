5817f693ff60d15257eb9dec7fc2b413f993279e
Mariusz Krzysztof Przybysz <mariusz.krzysztof.przybysz@gmail.com>
2020-01-27 10:39:30 +0100

    Init commit

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, ErrorHandler } from '@angular/core';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { GithubFollowersComponent } from './components/github-followers/github-followers.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { GithubProfileComponent } from './components/github-profile/github-profile.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostService } from './services/post.service';
+import { GithubFollowersService } from './services/github-followers.service';
+import { AppErrorHandler } from './common/errors/app-error-handler';
+import { CoursesService } from './services/courses.service';
+import { AuthorsService } from './services/authors.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GithubFollowersComponent,
+    GithubProfileComponent,
+    HomeComponent,
+    NotFoundComponent,
+    PostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    PostService,
+    CoursesService,
+    AuthorsService,
+    GithubFollowersService,
+    { provide: ErrorHandler, useClass: AppErrorHandler }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
